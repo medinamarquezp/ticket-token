7050ef6cabe18fd5613858f34557ee93013fa26d
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-29 00:10:45 +0200

    test: mint ticket test

--- test/TicketNFT.test.js
+++ test/TicketNFT.test.js
@@ -1,36 +1,67 @@
 const truffleAssert = require("truffle-assertions");
 const TicketNFT = artifacts.require("TicketNFT");
+const TicketMaster = artifacts.require("TicketMaster");
 
 contract("TicketNFT tests", (accounts) => {
-  let instance;
+  let ticketNFT, ticketMaster;
 
   before(async () => {
-    instance = await TicketNFT.deployed();
+    ticketNFT = await TicketNFT.deployed();
+    ticketMaster = await TicketMaster.deployed();
   });
+
+  const createTicket = async (from = accounts[1]) => {
+    const data = { from };
+    await ticketMaster.createOrganization(from, "Organization Test");
+    await ticketMaster.createEvent(
+      "Event test",
+      "Event description",
+      new Date().getDate(),
+      data
+    );
+    const createdEvent = await ticketMaster.getLastEvent(data);
+    await ticketMaster.createTicket(createdEvent.id, data);
+    return await ticketMaster.getLastTicket(createdEvent.id, data);
+  };
 
   it("Should set base URI", async () => {
     await truffleAssert.fails(
-      instance.setBaseURI("http://baseuri.com", {
+      ticketNFT.setBaseURI("http://baseuri.com", {
         from: accounts[1],
       }),
       truffleAssert.ErrorType.REVERT,
       "This operation is only available for owner address"
     );
-    await instance.setBaseURI("http://baseuri.com");
-    const baseUri = await instance.baseURI();
+    await ticketNFT.setBaseURI("http://baseuri.com");
+    const baseUri = await ticketNFT.baseURI();
     assert.equal(baseUri, "http://baseuri.com", "Should set base URI");
   });
 
   it("Should authorize an address", async () => {
     await truffleAssert.fails(
-      instance.authorize(accounts[1], true, {
+      ticketNFT.authorize(accounts[1], true, {
         from: accounts[1],
       }),
       truffleAssert.ErrorType.REVERT,
       "This operation is only available for owner address"
     );
-    await instance.authorize(accounts[1], true);
-    const isAuthorized = await instance.authorized(accounts[1]);
+    await ticketNFT.authorize(accounts[1], true);
+    const isAuthorized = await ticketNFT.authorized(accounts[1]);
     assert.equal(isAuthorized, true, "Should authorize an address");
+  });
+
+  it("Should mint a new ticket", async () => {
+    const from = accounts[1];
+    const ticket = await createTicket(from);
+    await truffleAssert.fails(
+      ticketNFT.mintTicket(ticket, {
+        from: accounts[2],
+      }),
+      truffleAssert.ErrorType.REVERT,
+      "This operation is only available for authorized addresses"
+    );
+    await ticketNFT.mintTicket(ticket);
+    const owner = await ticketNFT.ownerOf(ticket.id);
+    assert.equal(owner, from, "Should mint a new ticket");
   });
 });
