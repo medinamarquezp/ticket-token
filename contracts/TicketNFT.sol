d5302f7c7c553d9d31634c1848e9931eae30b086
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-28 20:59:14 +0200

    feat: ticket nft created

---
+++ contracts/TicketNFT.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Ticket} from "./Entities.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TicketNFT is ERC721 {
+    address public owner;
+    string public baseURI;
+    mapping(address => bool) authorized;
+
+    constructor() ERC721("TicketNFT", "TKTNFT") {
+        owner = msg.sender;
+    }
+
+    function authorize(
+        address _address,
+        bool _status
+    ) public onlyowner returns (bool) {
+        authorized[_address] = _status;
+        return true;
+    }
+
+    function mintTicket(
+        Ticket memory _ticket
+    ) public onlyauthorized returns (bool) {
+        _safeMint(_ticket.ownerAddress, _ticket.id);
+        return true;
+    }
+
+    function setBaseURI(
+        string memory _uri
+    ) public onlyowner returns (string memory) {
+        baseURI = _uri;
+        return baseURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    modifier onlyowner() {
+        require(
+            msg.sender == owner,
+            "This operation is only available for owner address"
+        );
+        _;
+    }
+
+    modifier onlyauthorized() {
+        require(
+            msg.sender == owner || authorized[msg.sender],
+            "This operation is only available for authorized addresses"
+        );
+        _;
+    }
+}
