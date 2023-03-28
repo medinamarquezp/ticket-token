e8ef54dbf237fd6d70c3c8da6a836a2b98a25da9
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-03-28 21:05:25 +0200

    chore: initial deployment

---
+++ migrations/01_ticketmaster_deployment.js
@@ -0,0 +1,16 @@
+const TKToken = artifacts.require("TKToken");
+const TicketNFT = artifacts.require("TicketNFT");
+const TicketMaster = artifacts.require("TicketMaster");
+
+module.exports = async function (deployer) {
+  // ERC20 token deployment
+  await deployer.deploy(TKToken);
+  const tkToken = await TKToken.deployed();
+  // ERC721 token deployment
+  await deployer.deploy(TicketNFT);
+  const ticketNFT = await TicketNFT.deployed();
+  // Main contract deployment
+  await deployer.deploy(TicketMaster, tkToken.address, ticketNFT.address);
+  const ticketmaster = await TicketMaster.deployed();
+  await ticketmaster.authorize(ticketNFT.address, true);
+};
